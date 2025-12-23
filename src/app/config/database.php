9ecbca9969042c0b9e342217ecc78256e6e0bd12
Carchedi <carchedi.ufjf@gmail.com>
2025-12-23 12:47:33 -0300

    Retira as credenciais do banco de dados e as coloca como variavel de ambiente

---
+++ src/app/config/database.php
@@ -0,0 +1,29 @@
+<?php
+
+$envPath = __DIR__ . '/../../../.env';
+
+if (!file_exists($envPath)) {
+    die('.env não encontrado');
+}
+
+$env = parse_ini_file($envPath);
+
+$host = $env['DB_HOST'];
+$port = $env['DB_PORT'];
+$dbname = $env['DB_NAME'];
+$user = $env['DB_USER'];
+$password = $env['DB_PASSWORD'];
+
+try {
+    $pdo = new PDO(
+        "pgsql:host=$host;port=$port;dbname=$dbname",
+        $user,
+        $password,
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]
+    );
+} catch (PDOException $e) {
+    die('Erro na conexão com o banco');
+}
